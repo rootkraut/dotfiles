a572aedb9dee873b559194b6bb085d5cc37ce9ae
Rouven Hernier <rootkraut@gmail.com>
2019-03-13 07:43:34 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-backup_root_folder="$HOME/dotfiles_backup"
+dotfiles_dir="~/dotfiles"
+backup_root_folder="~/dotfiles_backup"
 
 # Will check for backup root folder and create it if not present
 if [ ! -d ${backup_root_folder} ]; then
@@ -17,10 +18,9 @@ mkdir ${current_backup_folder}
 # Check for every .dotfile and create backup if needed
 #
 
-if [ -f $HOME/.xinitrc ]; then
+if [ -f ~/.xinitrc ]; then
     echo "'.xinitrc' found! Will create backup in '${current_backup_folder}..."
-    cp $HOME/.xinitrc ${current_backup_folder}
-    rm $HOME/.xinitrc
+    cp ~/.xinitrc ${current_backup_folder}
 fi
-ln -s ./.xinitrc $HOME/.xinitrc
+ln -sf ${dotfiles_dir}/.xinitrc ~/.xinitrc
 
